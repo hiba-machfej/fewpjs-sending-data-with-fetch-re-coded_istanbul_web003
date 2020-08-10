9ff3efcfcf0507685c6433a00f88b3bbeff0a535
Hiba Machfej <hiba.mashfij@gmail.com>
2020-08-10 12:37:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Add your code here
+document.addEventListener('DOMContentLoaded', function (e) {
+    e.preventDefault;
+    submitData("hiba", "hiba@gmail.com")
+ 
+});
+
+
+
+function submitData(userName, userEmail) {
+    
+    
+    const body = document.querySelector("body");
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    };
+    fetch("http://201.209.104.33:3000/users", configObj)
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+             const domCreate = document.createElement('P')
+             domCreate.innerHTML = 'hello';
+             body.appendChild(domCreate)
+
+        })
+
+        .catch(function(error){
+            alert("PROBLEM");
+            console.log(error.message);
+        });
+
+        
+
+
+
+}
+
+
